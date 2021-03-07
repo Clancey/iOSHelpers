014b3a0b3da0297d209979dc0a12e9e9dd410836
James Clancey <james.clancey@gmail.com>
2021-03-06 22:41:01 -0900

    Added CGRect

---
+++ iOSHelpers/Extensions/CGRectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace UIKit
+{
+	public static  class CGRectExtensions
+	{
+		public static CGRect ApplyInset(this CGRect rect, UIEdgeInsets inset)
+		{
+			rect.X += inset.Left;
+			rect.Y += inset.Top;
+			rect.Height -= inset.Top + inset.Bottom;
+			rect.Width -= inset.Left + inset.Right;
+			return rect;
+		}
+	}
+}
